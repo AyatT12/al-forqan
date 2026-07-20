3c3b759e02056eb152365a1afbcd41e0f277be06
AyatT12 <105029664+AyatT12@users.noreply.github.com>
2026-07-20 22:32:55 +0300

    Add files via upload

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -96,7 +96,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400&family=Cairo:wght@300;400;500;600;700;800&family=Fredoka:wght@400;500;600;700&family=Noto+Naskh+Arabic:wght@400;500;600;700&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Cairo:ital,wght@0,400;0,700;1,400&family=Cairo:wght@300;400;500;600;700;800&family=Fredoka:wght@400;500;600;700&family=Noto+Naskh+Arabic:wght@400;500;600;700&display=swap",
       },
     ],
   }),
